d3c93506e477b6f83164d916607daf3cfc615576
Krzysztof Pranczk <theowni@users.noreply.github.com>
2024-07-05 13:18:56 +0200

    Uncommenting X-Powered-By header

--- app/apis/healthcheck/service.py
+++ app/apis/healthcheck/service.py
@@ -5,5 +5,5 @@ router = APIRouter()
 
 @router.get("/healthcheck")
 def healthcheck(response: Response):
-    # response.headers["X-Powered-By"] = "Python 3.8, FastAPI ^0.103.0"
+    response.headers["X-Powered-By"] = "Python 3.8, FastAPI ^0.103.0"
     return {"ok": True}
