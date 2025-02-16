b7a969bcd3ee1fd2590ffe55f21af53af538fbcd
theowni <kpranczk7@gmail.com>
2025-02-16 22:30:42 +0100

    Added another vulnerable endpoint for orders

--- app/apis/orders/service.py
+++ app/apis/orders/service.py
@@ -1,9 +1,11 @@
 from apis.orders.services.create_order_service import router as create_order_router
 from apis.orders.services.get_order_service import router as get_order_router
 from apis.orders.services.get_orders_service import router as get_orders_router
+from apis.orders.services.get_orders_for_delivery_service import router as get_orders_for_delivery_route
 from fastapi import APIRouter
 
 router = APIRouter()
 router.include_router(create_order_router)
 router.include_router(get_order_router)
 router.include_router(get_orders_router)
+router.include_router(get_orders_for_delivery_router)
---
+++ app/apis/orders/services/get_orders_for_delivery_service.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from apis.orders import schemas
+from db.models import Order
+from db.session import get_db
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.orm import Session
+
+router = APIRouter()
+
+
+@router.get(
+    "/delivery/orders",
+    response_model=List[schemas.Order],
+    # we exclude this endpoint from the OpenAPI schema because
+    # it is not intended for public use!
+    include_in_schema=False,
+)
+def get_orders(
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_db),
+):
+    '''
+    This is a dedicated endpoint for delivery services to integrate with 
+    the Restaurant. Delivery services can use this endpoint to get a list of 
+    latest orders with their details.
+    '''
+    orders = (
+        db.query(Order)
+        .order_by(Order.date_ordered.desc())
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
+    return orders
